d49353344c50517c94c216f8dbe97327c2971837
Severus-Matthew <drprofmjha@gmail.com>
2023-02-28 04:04:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
 import flow from "./pr1.png";
+import ex1 from "./ex1.png";
+import ex2 from "./ex2.png";
 
 
 function App() {
@@ -34,20 +36,48 @@ function App() {
                       of the scene. The proposed scheme is then packaged into a web app to aid the concerns of dark
                       image enhancement in daily life of consumers. Extensive quantitative and qualitative analysis
                       validates the superiority of NSDIE over various state-of-art techniques.</p>
-                  <p class="title" style={{textAlign:"left", padding:"2%"}}>Index Terms - Image enhancement, nighttime scenes, multiscale retinex model, camera response function.</p>
+                  <p class="title" style={{ textAlign: "left", padding: "2%" }}>Index Terms - Image enhancement, nighttime scenes,
+                      multiscale retinex model, camera response function.</p>
                                  
               </div>
               <div class="card">
-                  <p style={{ color: "white", textAlign: "center" }}><button style={{ fontSize: "30pt", fontFamily: "Times New Roman" }}>PROPOSED METHOD</button></p>
+                  <p style={{ color: "white", textAlign: "center" }}><button style={{ fontSize: "30pt", fontFamily: "Times New Roman" }}>
+                      PROPOSED METHOD</button></p>
                   <img src={flow} style={{padding: "2%" }} alt="Flow-Chart of proposed Method" className="img-fluid" />
               </div>
               <div class="card">
-                  <p style={{ color: "white", textAlign: "center" }}><button style={{ fontSize: "30pt", fontFamily: "Times New Roman" }}>DOWNLOAD MATLAB APP</button></p>
-                  <p style={{ fontFamily: "Times New Roman", textAlign: "justify", padding: "2%", paddingBottom: "0" }}>Download the .zip folder containing the Matlab App by clicking on the option below.
+                  <p style={{ color: "white", textAlign: "center" }}><button style={{ fontSize: "30pt", fontFamily: "Times New Roman" }}>
+                      EXAMPLES</button></p>
+                  <img src={ex1} style={{ padding: "2%" }} alt="Example of proposed Method" className="img-fluid" />
+                  <img src={ex2} style={{ padding: "2%" }} alt="Example of proposed Method" className="img-fluid" />
+                  
+              </div>
+              <div class="card">
+                  <p style={{ color: "white", textAlign: "center" }}><button style={{ fontSize: "30pt", fontFamily: "Times New Roman" }}>
+                      DOWNLOAD MATLAB APP</button></p>
+                  <p style={{ fontFamily: "Times New Roman", fontSize: "15pt", textAlign: "justify", padding: "2%", paddingBottom: "0" }}>
+                      Download the ..mlappinstall folder containing the Matlab App by clicking on the option below.
                       <br /> The Download should begin instantly.
-                      <br /> After downlading completes, unzip the folder and locate, "for_redistribution_only_files". <br /> Run the .exe file inside the folder to add our algorithm as a MATLAB app. <br /> For users who do not have MATLAB runtime, Run the .exe file inside folder namned
-                      "for_redistribution".                  </p>
-                  <p style={{ color: "white", textAlign: "center" }}><button style={{ fontSize: "30pt", fontFamily: "Times New Roman" }}>DOWNLOAD ZIP File</button></p>
+                      <br /> After downlading completes, locate the file through an ongoing MatLab session <br />
+                                           Upon opening it through a MatLab session, a promt will pop-up asking for installation. This will take about a minute.<br />
+                      After the installation completes, the app gets added to the APPS section in MatLab environment and can be used
+                      anytime by clicking on the icon.
+                                 </p>
+                  <p style={{ color: "white", textAlign: "center" }}><button2 style={{ fontSize: "30pt", color: "black", background: "white", fontFamily: "Times New Roman" }}>
+                      <a href="NSDIE_APP_1.mlappinstall" download>DOWNLOAD .mlappinstall File</a></button2></p>
+              </div>
+              <div class="card">
+                  <p style={{ color: "white", textAlign: "center" }}><button style={{ fontSize: "30pt", fontFamily: "Times New Roman" }}>
+                      DOWNLOAD STANDALONE APP</button></p>
+                  <p style={{ fontFamily: "Times New Roman", fontSize:"15pt", textAlign: "justify", padding: "2%", paddingBottom: "0" }}>
+                      Download the .zip folder containing the Matlab App by clicking on the option below.
+                      <br /> The Download should begin instantly.
+                      <br /> After downlading completes, unzip the folder and locate, "for_redistribution_only_files". <br />
+                      Run the .exe file inside the folder to install and open the stanadlone app for the proposed algorithm.. <br /> For users who do not have
+                      MATLAB runtime, Run the .exe file inside folder namned
+                      "for_redistribution" first and then proceed as the previous step.                  </p>
+                  <p style={{ color: "white", textAlign: "center" }}><button2 style={{ fontSize: "30pt",color:"black", background:"white",fontFamily: "Times New Roman" }}>
+                      <a href="NSDIE_APP.zip" download> DOWNLOAD .zip File</a></button2></p>
               </div>
               
       </header>
